6678b526e4f14a9e3ca1d606e3eb4c10426d800a
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2024-12-02 14:54:33 +0100

    Mobile opt for press.

--- app/components/column-arrangement.tsx
+++ app/components/column-arrangement.tsx
@@ -12,17 +12,18 @@ export function ColumnArrangement(props: ColumnArrangementProps): ReactElement {
   const isRightAligned = alignment === "right";
 
   const columnClass = clsx({
-    "grid-cols-2": adjustment === "1:1",
-    "grid-cols-[2fr_1fr]":
+    "grid-cols-1": true,
+    "md:grid-cols-2": adjustment === "1:1",
+    "md:grid-cols-[2fr_1fr]":
       (adjustment === "2:1" && !isRightAligned) ||
       (adjustment === "1:2" && isRightAligned),
-    "grid-cols-[1fr_2fr]":
+    "md:grid-cols-[1fr_2fr]":
       (adjustment === "1:2" && !isRightAligned) ||
       (adjustment === "2:1" && isRightAligned),
-    "grid-cols-[2fr_3fr]":
+    "md:grid-cols-[2fr_3fr]":
       (adjustment === "2:3" && !isRightAligned) ||
       (adjustment === "3:2" && isRightAligned),
-    "grid-cols-[3fr_2fr]":
+    "md:grid-cols-[3fr_2fr]":
       (adjustment === "3:2" && !isRightAligned) ||
       (adjustment === "2:3" && isRightAligned),
   });
@@ -31,13 +32,13 @@ export function ColumnArrangement(props: ColumnArrangementProps): ReactElement {
     <div className={`grid ${columnClass} gap-4`}>
       {isRightAligned ? (
         <>
-          <div>{children[1]}</div>
-          <div>{children[0]}</div>
+          <div className="md:order-2">{children[1]}</div>
+          <div className="md:order-1">{children[0]}</div>
         </>
       ) : (
         <>
-          <div>{children[0]}</div>
-          <div>{children[1]}</div>
+          <div className="md:order-2">{children[0]}</div>
+          <div className="md:order-1">{children[1]}</div>
         </>
       )}
     </div>
