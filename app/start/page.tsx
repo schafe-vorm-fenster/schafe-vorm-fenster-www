ebd437ecc5969963e0c38b1fc4322306b4b7087e
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2025-03-31 13:22:43 +0200

    Replaced web form with google form.

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import PageTitle from "../components/page-title";
 import Section from "../components/section";
-import { NewLeadForm } from "./form";
 
 export const metadata: Metadata = {
   title: "Kalender anmelden und Termine veröffentlichen",
@@ -20,8 +19,15 @@ export default function Start() {
   return (
     <>
       <PageTitle text="Jetzt loslegen. So geht's." />
-      <Section color="black">
-        <NewLeadForm />
+      <Section color="green">
+        <div className="w-full flex justify-center items-center">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdO5AlaOpDWGwukrIge-qPvXAeiEVMgEwAViC-CmvkWLAjL3g/viewform?embedded=true"
+            width="640"
+            height="1900"
+            className="max-w-full"
+          ></iframe>
+        </div>
       </Section>
     </>
   );
